7064d9084d1d61a324a144d5a778a3c07c096f9b
Alex Yocom-Piatt <ay-p@decred.org>
2016-08-30 16:45:00 -0500

    Add heightCheck to make sure that purchase is run once per height (#44)
    Fixes #35

--- buyer.go
+++ buyer.go
@@ -104,6 +104,7 @@ type ticketPurchaser struct {
 	maintainMinPrice    bool         // Flag for minimum price manipulation
 	useMedian           bool         // Flag for using median for ticket fees
 	priceMode           avgPriceMode // Price mode to use to calc average price
+	heightCheck         map[int64]struct{}
 }
 
 // newTicketPurchaser creates a new ticketPurchaser.
@@ -156,12 +157,23 @@ func newTicketPurchaser(cfg *config,
 		maintainMinPrice: maintainMinPrice,
 		useMedian:        cfg.FeeSource == useMedianStr,
 		priceMode:        priceMode,
+		heightCheck:      make(map[int64]struct{}),
 	}, nil
 }
 
 // purchase is the main handler for purchasing tickets for the user.
 // TODO Not make this an inlined pile of crap.
 func (t *ticketPurchaser) purchase(height int64) error {
+
+	// Check to make sure that the current height has not already been seen
+	// for a reorg or a fork
+	if _, exists := t.heightCheck[height]; exists {
+		log.Tracef("We've already seen this height, "+
+			"reorg/fork detected at height %v", height)
+		return nil
+	}
+	t.heightCheck[height] = struct{}{}
+
 	// Initialize webserver update data. If the webserver is
 	// enabled, defer a function that writes this data to the
 	// disk.
